84266f52a6bcc3f35a68820993218c10be50b0a0
Darius Fang <dariusfang1@gmail.com>
2024-10-01 15:54:20 -0600

    zoom in

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera FPSCam;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+    bool zoomedInToggle = false;
+    private void Update() {
+        if(Input.GetMouseButton(1))
+        {
+            if(zoomedInToggle == false)
+            {   
+                zoomedInToggle = true;
+                FPSCam.fieldOfView = zoomedInFOV;
+            }
+            else
+            {
+                zoomedInToggle = false;
+                FPSCam.fieldOfView = zoomedOutFOV;
+            }
+        }
+    }
+}
